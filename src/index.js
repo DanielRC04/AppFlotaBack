450c9739f6efba84127ba387b226f9146120e7f9
drodriguezA4 <d.rodriguez@abracol.com>
2024-05-24 14:31:27 -0500

    Se agrega lo necesario para comenzar un servidor con express

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+const fs = require('fs');
+dotenv.config();
+
+//ROUTES
+const routerUsers = require("./routes/adminUsers");
+const routerCars = require("./routes/adminCars");
+const routerRents = require("./routes/adminRents");
+
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.use(express.json({ limit: '150mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: false, parameterLimit: 50000 }));
+
+//rutas estaticas
+app.use(express.json({ limit: "150mb" }));
+
+app.use(
+  express.urlencoded({ limit: "50mb", extended: false, parameterLimit: 50000 })
+);
+
+routerUsers(app);
+routerCars(app);
+routerRents(app);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT => { console.log(`🚀 Server listening on port ${PORT} y esta en modo: ${process.env.NODE_ENV}`)});
